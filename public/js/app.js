995a8ec003215e4a4008adcf7dd9404fe213e85d
TomasJ1993 <tomastomasj@gmail.com>
2019-03-18 15:19:01 +0800

    remove unnecessary console.log comment

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,4 @@
 
-console.log('js file is loaded');
-
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
